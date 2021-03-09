38a269f7393028c5e34a322ee4966bfe0cff7df1
KGrzeg <kupczykgrzeg@gmail.com>
2021-03-09 17:34:56 +0100

    Store cache in database

--- app.js
+++ app.js
@@ -2,54 +2,94 @@ require('dotenv').config()
 const express = require('express');
 const proxy = require('express-http-proxy');
 const moment = require('moment');
+const MongoClient = require('mongodb').MongoClient;
 
 const port = process.env.PORT;
 const baseUrl = process.env.PROXY_TARGET;
 const cacheDuration = process.env.CACHE_DURATION_HOURS;
-const cache = {};
+const mongoUrl = process.env.MONGO_URL;
+const mongoDb = process.env.DATABASE;
+const collectionName = "cache";
 const app = express();
 
-app.use('/api/', proxy(baseUrl, {
-  filter: function (req, res) {    
-    if (cache[req.url] === undefined){
-      console.log(`First call to ${req.url} [proxy]`);
-      return true;
-    }
-    
-    const cached = cache[req.url];
-    const diffInHours = moment().diff(cached.timestamp, "hours");
+const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    if (diffInHours >= cacheDuration){
-      console.log(`${cacheDuration} hours passed [proxy]`);
-      return true;
-    }
-    
-    console.log(`Cached ${diffInHours} hours ago [cache]`);
-    res.send(cached.content);
-    return false;
-  },
-  userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
-    let body;
-    try{
-      body = JSON.parse(proxyResData.toString('utf8'));
-    }catch(err){
-      console.log(err);
-      return {};
-    }
+function connectToDatabase() {
+  return new Promise((res, rej) => {
+    client.connect(async err => {
+      if (err) {
+        console.log("Connection error");
+        return rej(err);
+      }
 
-    cache[userReq.url] = {
-      content: body,
-      timestamp: moment()
-    };
+      const collection = client.db(mongoDb).collection(collectionName);
+      console.log("Connected to db");
+      return res(collection);
+    });
+  })
+}
 
-    return cache[userReq.url].content;
-  }
-}));
 
 app.get('/', (req, res) => {
   res.send('Go away!')
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+async function main() {
+  const collection = await connectToDatabase();
+
+  app.use('/api/', proxy(baseUrl, {
+    filter: async function (req, res) {
+      const cached = await collection.findOne({ url: req.url });
+
+      if (cached === null) {
+        console.log(`First call to ${req.url} [proxy]`);
+        return true;
+      }
+
+      const timestamp = moment(cached.timestamp)
+      const diffInHours = moment().diff(timestamp, "hours");
+
+      if (diffInHours >= cacheDuration) {
+        console.log(`${cacheDuration} hours passed [proxy]`);
+        return true;
+      }
+
+      console.log(`Cached ${diffInHours} hours ago [cache]`);
+      res.send(cached.response);
+      return false;
+    },
+    userResDecorator: async function (proxyRes, proxyResData, userReq, userRes) {
+      let body;
+      try {
+        body = JSON.parse(proxyResData.toString('utf8'));
+
+        console.log("Try to update cache with url", userReq.url);
+        await collection.updateOne({ url: userReq.url }, {
+          $set: {
+            timestamp: new Date(),
+            response: JSON.stringify(body)
+          }
+        }, {
+          upsert: true
+        });
+        console.log("Updated cache");
+      } catch (err) {
+        body = { error: err.toString() };
+      }
+
+      return body;
+    }
+  }));
+
+
+  const closeDbClient = (code) => client.close();
+  process.on('exit', closeDbClient);
+  process.on('SIGINT', closeDbClient);
+  process.on('SIGTERM', closeDbClient);
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
+}
+
+main();
